cdae1cccfc20789d2afd922d4f024e4b8bf20c4a
duchunter <duc71098@gmail.com>
2019-06-15 20:01:02 +0700

    new workable

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get("/", function(req, res, next) {
 router.post("/signup", db.signUp);
 router.post("/login", db.logIn);
 
+router.get('/admin/users', db.verifyToken, db.getUser);
+router.delete('/admin/users/:id', db.verifyToken,db.deleteUser);
+router.put('/password', db.verifyToken,db.changePassword);
+
 router.get("/mangas", db.getManga);
 router.get("/mangas/subscribed", db.verifyToken, db.getFavoriteManga);
 router.get("/mangas/:id", db.getMangaById);
@@ -19,26 +23,18 @@ router.put("/mangas/:id", db.verifyToken, db.updateManga);
 router.put("/mangas/:mangaid/rating", db.verifyToken, db.addRating);
 router.put("/mangas/:mangaid/subscription", db.verifyToken, db.addFavorite);
 
-router.get("/mangas/:id/chapters", db.getChapters);
-router.post("/mangas/:id/chapters", db.verifyToken, db.addChapter);
-router.get("/mangas/:mangaid/chapters/:chapterid", db.getChapterById);
-router.put(
-  "/mangas/:mangaid/chapters/:chapterid",
-  db.verifyToken,
-  db.updateChapter
-);
-router.delete(
-  "/mangas/:mangaid/chapters/:chapterid",
-  db.verifyToken,
-  db.deleteChapter
-);
-router.get("/mangas/:mangaid/chapters/:chapterid/comments", db.getComment);
-router.post(
-  "/mangas/:mangaid/chapters/:chapterid/comments",
-  db.verifyToken,
-  db.addComment
-);
+router.get('/mangas/:id/chapters',db.getChapters)
+router.post('/mangas/:id/chapters',db.verifyToken, db.addChapter);
+router.get('/mangas/:mangaid/chapters/:chapterid', db.getChapterById);
+router.put('/mangas/:mangaid/chapters/:chapterid',db.verifyToken, db.updateChapter);
+router.delete('/mangas/:mangaid/chapters/:chapterid',db.verifyToken, db.deleteChapter);
+router.get('/mangas/:mangaid/chapters/:chapterid/comments', db.getComment);
+router.post('/mangas/:mangaid/chapters/:chapterid/comments',db.verifyToken, db.addComment);
 
-router.get("/genres", db.getGenre);
+router.put('/password',db.verifyToken,db.changePassword);
+router.get('/admin/users',db.verifyToken,db.getUser);
+router.delete('/admin/users/:id', db.verifyToken,db.deleteUser);
+router.get('/info',db.verifyToken,db.getInfo);
+router.get('/genres', db.getGenre);
 
 module.exports = router;
